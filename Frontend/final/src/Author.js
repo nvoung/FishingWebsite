29815ef42facc91f6a190ea04b03c1ec0e02eb20
dchism03 <dchism3602@gmail.com>
2024-12-11 19:42:18 -0600

    author page

--- Frontend/final/src/Author.js
+++ Frontend/final/src/Author.js
@@ -58,6 +58,42 @@ const Author = () => {
 
         <hr className="featurette-divider" />
       </Container>
+
+      <Container className="mt-5">
+        
+        <Row className="featurette">
+          <Col md={7} className="order-md-2">
+            <h2 style={{ textDecoration: 'underline', textAlign: 'center', marginRight: '120px' }}>Doyle Chism</h2>
+            <p style={{
+              marginTop: '50px',
+              textAlign: 'center',
+              backgroundColor: 'lightgrey',
+              display: 'inline-block',
+              width: '800px',
+              paddingTop: '25px',
+              paddingBottom: '25px',
+              borderRadius: '25px'
+            }}>
+              My name is Doyle Chism and I created this webiste in order to display products I am interested in 
+              and portay it to others. I wanted to stregthen my knowledge of developing a full-stack application
+              and also integrate hobbies I am interested in.<br />
+              <strong>Course:</strong> 3090 Construction Of User Interfaces <br />
+              <strong>Professor Name:</strong> Abraham Aldaco <br />
+              <strong>Author email:</strong> dchism03@iastate.edu <br />
+              <strong>Date:</strong> December 10, 2024 <br />
+            </p>
+          </Col>
+          <Col md={5} className="order-md-1">
+            <img
+              src="/photos/profileDoyle.png"
+              alt="Profile Picture"
+              style={{ marginLeft: '75px', borderRadius: '50%', paddingRight: '0px' }}
+            />
+          </Col>
+        </Row>
+
+        <hr className="featurette-divider" />
+      </Container>
     </div>
   );
 };
